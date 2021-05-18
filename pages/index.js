644a272a143c86a8adf9f3702489e6feee0fdc6b
Julio Andres Silva Vasquez <jsilva@ibm.com>
2021-05-18 15:25:29 -0500

    Changed index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      <h1>Hello World from Julio Silva to test Code Enginew.{' '}</h1>
+      <h1>Hello World from Julio Silva to test Code Engine.{' '}</h1>
       <Link href="/about">
         <a>About</a>
       </Link>
